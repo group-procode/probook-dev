4cdf1705073996ff96c3dc39ce3c3d21f9402c61
llicar <lucas.licar.sousa@gmail.com>
2021-02-26 00:17:41 -0300

    adicionando arquivo de configuracao do knex

---
+++ backend/knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: "d649gr3avmaif6",
+      user: "jvwgnajtjuyhud",
+      password: "26c2946448e02fbe98ef654b6ef80ac5e37f732fa619ae5c0ccb1c60d6c4aa8c"
+    }
+  }
+}
+
+
