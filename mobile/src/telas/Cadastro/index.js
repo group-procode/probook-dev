dd930c6cdc9c105b912c9edfc29dc3aa25bd2a99
Vitor Couto <coutovitor11@gmail.com>
2021-03-09 17:55:27 -0300

    cria estrutura da tela [WIP]

---
+++ mobile/src/telas/Cadastro/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export class Cadastro extends Component {
+    render() {
+        return (
+            <View>
+                <Text> ola </Text>
+            </View>
+        );
+    }
+}
+
+export default Cadastro;
