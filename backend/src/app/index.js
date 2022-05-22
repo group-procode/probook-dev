67db3c1246813035afa520c4fadd517eeff4cc2d
rafaell-alves <rafaell.alves16062002@gmail.com>
2022-05-21 22:56:29 -0300

    endpoint para tela de de livro

--- backend/src/app/index.js
+++ backend/src/app/index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import leitorController from "./controllers/leitorController.js";
 import faculdadeController from "./controllers/faculdadeController.js";
-
+import livroController from './controllers/livroController.js';
 // Responsável por pegar as variáveis de ambiente
 dotenv.config();
 
@@ -12,10 +12,12 @@ const app = express();
 // Diz ao node que vamos trabalhar com json
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+//Rota Livros
+app.use("/livros", livroController);
 // Agrupando as rotas de leitor
 app.use("/leitor", leitorController);
 //Rota Faculdades
 app.use("/faculdades", faculdadeController);
+
 // Inicia o servidor na porta 5000
 app.listen(5000, () => console.log("Aplicação está no ar..."));
